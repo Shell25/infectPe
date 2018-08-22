44fbd8cf8cbbdd46ebca6f8b8925768fcc6e4cc2
Shell25 <40403803+Shell25@users.noreply.github.com>
2018-08-22 13:49:19 +0430

    Update PE.h

--- infectPe_x86/PE.h
+++ infectPe_x86/PE.h
@@ -1,8 +1,10 @@
 #pragma once
-#include <Windows.h>
 #include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+#include "PE.h"
+#include <iostream>
 #pragma warning(disable : 4996)
-
 
 typedef struct
 {
@@ -16,6 +18,14 @@ typedef struct
 	DWORD					dwImageSizeOnDisk;
 } PE_Info;
 
-void writeToFile(PE_Info, PE_Info, int, char*);
-void init(FILE *, PE_Info *, CHAR *);
-void addSection(PE_Info, int, char *);
+union HexToChar
+{
+	unsigned int twoChar;
+	unsigned char character[2];
+};
+
+void changEntryPoint(PE_Info *);
+void writeToFile(PE_Info );
+void init(FILE *, PE_Info *);
+void disableASLR(PE_Info *);
+void addSection(int, PE_Info, PE_Info *, IMAGE_SECTION_HEADER *);
